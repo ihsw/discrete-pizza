4fd803b9cd9f675615aa930d9479875f4c25c380
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-02-16 14:58:08 -0500

    Fix typo in api endpoint uri.

--- src/Api.ts
+++ src/Api.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
 
-export const apiEndpoint = 'http://core-graphql.dev.waldo.photos/pizza';
+export const apiEndpoint = 'https://core-graphql.dev.waldo.photos/pizza';
 export const client = new ApolloClient({
     link: new HttpLink({ uri: apiEndpoint }),
     cache: new InMemoryCache()
