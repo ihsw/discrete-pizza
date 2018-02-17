56bd42d6b459fb0e57925d0a2099679c67756de2
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-02-17 13:22:42 -0500

    Fix api test to use mock data.

--- src/Api.test.ts
+++ src/Api.test.ts
@@ -1,4 +1,5 @@
-import { getDefaultApi, apiEndpoint } from './Api';
+import { getTestApi, apiEndpoint } from './Api';
+import { PizzaSize, PizzaToppingField, PizzaTopping } from './types';
 
 describe('Api', () => {
     it('Has an api endpoint', () => {
@@ -6,8 +7,50 @@ describe('Api', () => {
     });
     
     it('Queries the api', async () => {
-        const api = getDefaultApi();
+        const typeDefs = `
+        type Topping {
+            name: String
+            price: Float
+        }
+        type ToppingField {
+            defaultSelected: Boolean
+            topping: Topping
+        }
+        type PizzaSize {
+            name: String
+            basePrice: Float
+            maxToppings: Int
+            toppings: [ToppingField]
+        }
+        type Query {
+            pizzaSizes: [PizzaSize]
+        }
+        schema {
+            query: Query
+        }
+        `;
+        const mockPizzaSizes = [<PizzaSize> {
+            name: 'ayy',
+            basePrice: 99,
+            maxToppings: 5,
+            toppings: [
+                <PizzaToppingField> {
+                    defaultSelected: false,
+                    topping: <PizzaTopping> {
+                        name: 'lmao',
+                        price: 99
+                    }
+                }
+            ]
+        }];
+        const mocks = {
+            Query: () => ({
+                pizzaSizes: () => mockPizzaSizes
+            })
+        };
+        const api = getTestApi(typeDefs, mocks);
+
         const data = await api.getPizzaSizes();
-        expect(data.pizzaSizes.length).toBeGreaterThan(0);
+        expect(data.pizzaSizes).toMatchObject(mockPizzaSizes);
     });
 });
