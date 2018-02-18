cb6bc290f71cb0deb936c3f2951c5a460a709304
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-02-17 19:52:08 -0500

    Fix capitalization stupidity.

--- src/components/PizzaSizes.tsx
+++ src/components/PizzaSizes.tsx
@@ -1,10 +1,10 @@
-import * as react from 'react';
+import * as React from 'react';
 
 export interface Props {
     name: string;
 }
 
-export class PizzaSizes extends react.Component<Props> {
+export class PizzaSizes extends React.Component<Props> {
     render() {
         return (
             <div className="greeting">Hello, {this.props.name}</div>
