b60903340525dab827b7501a23e651035629226f
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-02-18 11:41:11 -0500

    Add pizza-sizes container to index.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import { pizzaSizes } from './reducers';
+import { StoreState } from './types';
+import PizzaSizes from './containers/PizzaSizes';
 import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
+const preloadedState: StoreState = {
+  loading: false,
+  pizzaSizes: []
+};
+const store = createStore<StoreState>(pizzaSizes, preloadedState, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <PizzaSizes name="Adrian" />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
