930c863bec403a55204167a535d9aa99ecf79b25
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-02-17 20:51:56 -0500

    Add fetch-pizza-sizes func prop.

--- src/components/PizzaSizes.test.tsx
+++ src/components/PizzaSizes.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { configure, shallow } from 'enzyme';
+import * as sinon from 'sinon';
 import * as Adapter from 'enzyme-adapter-react-16';
 
 import { PizzaSizes } from './PizzaSizes';
@@ -8,7 +9,9 @@ configure({ adapter: new Adapter() });
 
 describe('PizzaSizes', () => {
     it('Renders correctly', () => {
-        const pizzaSizes = shallow(<PizzaSizes name="Adrian" />);
+        const spy = sinon.spy();
+        const pizzaSizes = shallow(<PizzaSizes name="Adrian" fetchPizzaSizes={spy} />);
         expect(pizzaSizes.find('.greeting').text()).toEqual('Hello, Adrian');
+        expect(spy.calledOnce).toBe(true);
     });
 });
--- src/components/PizzaSizes.tsx
+++ src/components/PizzaSizes.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 
 export interface Props {
     name: string;
+    fetchPizzaSizes: () => void;
 }
 
 export class PizzaSizes extends React.Component<Props> {
+    componentDidMount() {
+        this.props.fetchPizzaSizes();
+    }
+
     render() {
         return (
             <div className="greeting">Hello, {this.props.name}</div>
